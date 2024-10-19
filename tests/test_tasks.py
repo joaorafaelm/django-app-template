182bab0c51c2fd07a552eab38c6e15bb41357d0c
João Rafael <joaoraf@me.com>
2024-10-19 17:05:38 -0300

    test: remove commented code from test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,3 +2,11 @@ from app.tasks import run_healthchecks
 
 def test_run_healthchecks(app):
     run_healthchecks.defer(1)
+
+# def test_model():
+    
+#     from mixer.backend.django import mixer
+#     from customapp.models import User, UserMessage
+
+#     # Generate a random user
+#     user = mixer.blend(User)
